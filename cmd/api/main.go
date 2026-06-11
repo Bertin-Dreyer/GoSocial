afcfa5193ee36859e246b203a16112a2f998ab27
Berti <berti@example.com>
2026-06-11 12:09:26 +0200

    feat: load ADDR from environment variable with default :8080

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,22 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Bertin-Dreyer/go-social/internal/env"
+)
 
 func main() {
 	app := &application{
 		config: config{
-			addr: ":8080",
+			addr: env.GetString("ADDR", ":8080"),
 		},
 	}
 
 	mux := app.mount()
 
 	log.Fatal(app.run(mux))
+	fmt.Println("ADDR from env:", os.Getenv("ADDR"))
 }
